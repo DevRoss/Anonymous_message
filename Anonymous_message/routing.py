5ed85b375afe115293b4377097a5206168c51c85
ross <820584458@qq.com>
2017-08-21 11:45:46 +0800

    Modify routing

--- Anonymous_message/routing.py
+++ Anonymous_message/routing.py
@@ -12,7 +12,7 @@ from channels.routing import route
 from chat.consumers import ws_connect, ws_message, ws_disconnect
 
 channel_routing = [
-    route("websocket.connect", ws_connect, path=r"^/chat/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.receive", ws_message, path=r"^/chat/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.disconnect", ws_disconnect, path=r"^/chat/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    route("websocket.connect", ws_connect, path=r"^/anonymous-chat/(?P<room_name>[a-zA-Z0-9_]+)/?$"),
+    route("websocket.receive", ws_message, path=r"^/anonymous-chat/(?P<room_name>[a-zA-Z0-9_]+)/?$"),
+    route("websocket.disconnect", ws_disconnect, path=r"^/anonymous-chat/(?P<room_name>[a-zA-Z0-9_]+)/?$"),
 ]
